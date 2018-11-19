c8324706da5d82771c3a15ec77e3b2ccac97e662
Thomas Pearce <etonix@shadowed-souls.net>
2018-11-19 00:23:32 -0600

    Update maid.php
    Updated in primary project

--- maid.php
+++ maid.php
@@ -31,10 +31,32 @@
       return $this->_result;
     }
     
+    public function isArray($data) {
+      
+     // RECURSIVE JSON ARRAY  REGEX 
+      $regexString = '"([^"\\\\]*|\\\\["\\\\bfnrt\/]|\\\\u[0-9a-f]{4})*"';
+      $regexNumber = '-?(?=[1-9]|0(?!\d))\d+(\.\d+)?([eE][+-]?\d+)?';
+      $regexBoolean= 'true|false|null'; 
+      $regex = '/\A('.$regexString.'|'.$regexNumber.'|'.$regexBoolean.'|';    //string, number, boolean
+      $regex.= '\[(?:(?1)(?:,(?1))*)?\s*\]|'; //arrays
+      $regex.= '\{(?:\s*'.$regexString.'\s*:(?1)(?:,\s*'.$regexString.'\s*:(?1))*)?\s*\}';    //objects
+      $regex.= ')\Z/is'; 
+
+      if(is_array($data) && $this->_is_assoc($data)) // ASSOCIATIVE ARRAY
+          return 4;
+      else if(is_array($data) && $this->_is_multi($data)) // MULTIDIMENSIONAL ARRAY
+          return 2;
+      else if(is_array($data)) // ARRAY
+          return 1;
+      else if(preg_match($regex, (string)$data) == 1) // JSON DATA
+          return 3;
+      else
+          return 0; // STRING
+    }
     
     private function _sanitize($data) {
         
-      switch($this->_isArray($data)) {
+      switch($this->isArray($data)) {
           
         case 0:
            $this->_prepSingle($data);
@@ -68,32 +90,9 @@
       }
     }
     
-    private function _isArray($data) {
-      
-     // RECURSIVE JSON ARRAY  REGEX 
-      $regexString = '"([^"\\\\]*|\\\\["\\\\bfnrt\/]|\\\\u[0-9a-f]{4})*"';
-      $regexNumber = '-?(?=[1-9]|0(?!\d))\d+(\.\d+)?([eE][+-]?\d+)?';
-      $regexBoolean= 'true|false|null'; 
-      $regex = '/\A('.$regexString.'|'.$regexNumber.'|'.$regexBoolean.'|';    //string, number, boolean
-      $regex.= '\[(?:(?1)(?:,(?1))*)?\s*\]|'; //arrays
-      $regex.= '\{(?:\s*'.$regexString.'\s*:(?1)(?:,\s*'.$regexString.'\s*:(?1))*)?\s*\}';    //objects
-      $regex.= ')\Z/is'; 
-
-      if(is_array($data) && $this->_is_assoc($data)) // ASSOCIATIVE ARRAY
-          return 4;
-      else if(is_array($data) && $this->_is_multi($data)) // MULTIDIMENSIONAL ARRAY
-          return 2;
-      else if(is_array($data)) // ARRAY
-          return 1;
-      else if(preg_match($regex, (string)$data) == 1) // JSON DATA
-          return 3;
-      else
-          return 0; // STRING
-    }
-    
     private function _prepSingle($data) {
        
-      if(!$this->_isArray($data)) {
+      if(!$this->isArray($data)) {
         $val = addslashes($data);
         $val = implode("", explode("\\", $val));
         $val = stripslashes(trim($val));
@@ -120,10 +119,10 @@
     
     private function _prepArray($data) {
       
-      if($this->_isArray($data) > 0) {
+      if($this->isArray($data) > 0) {
         
         foreach($data as $key=>$value) {
-          if($this->_isArray($value)) { 
+          if($this->isArray($value)) { 
             $this->_sanitize($key);
             $key = $this->_result;
             $this->_sanitize($value);
